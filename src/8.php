0eb9ce953d1f5d1406c79f5880312ad81ccc77d6
redfeardanek770 <redfeardanek770@gmail.com>
2025-03-15 18:22:30 +0000

    Add 8.php

---
+++ src/8.php
@@ -0,0 +1,14 @@
+
+<?php
+
+// Function to generate a random number between 1 and 10
+function getRandomNumber() {
+    return rand(1, 10);
+}
+
+// Function to check if a number is even or odd
+function isEven($number) {
+    return $number % 2 === 0;
+}
+
+?>
