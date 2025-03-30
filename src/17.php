85e3abeae339caaf52f47536ff3f7083a7ff7449
redfeardanek770 <redfeardanek770@gmail.com>
2025-03-30 14:20:18 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,21 @@
+<?php
+// Define your database connection
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Example PHP code here
+
+$conn->close();
+
+// Close the database connection
+?>
